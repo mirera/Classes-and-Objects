9981f48cddba5dce7c940ed56cafe29040cb35e0
Enigma mirera <mirera.moses@gmail.com>
2022-06-01 19:05:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ class Student:
         self.student_tracks = tracks
         self.student_score = score
 
+    def change_name(self, new_name):
+        self.student_name = new_name
 
+    def change_age(self, new_age):
+        self.student_age = int(new_age)
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+    def add_track(self, new_track):
+        self.student_tracks.append(new_track) 
+        
+    def get_score(self):
+        return self.student_score
+
+#test with the codes below
+
+Bob = Student("Bob",26, ["FE","BE"], 20.90)
+print(Bob.student_tracks)
+Bob.add_track('WE')
+print(Bob.student_tracks)
+#Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+#Bob.change_name("Peter")
+#Bob.change_age(34)
+#Bob.add_track("UI/UX")
+#Bob.get_score()
